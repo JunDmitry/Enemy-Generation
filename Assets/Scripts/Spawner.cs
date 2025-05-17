884eb178cac3971f2a89336805fa508e226256ea
Dmitry Rysev <100911164+JunDmitry@users.noreply.github.com>
2025-05-17 19:28:03 +0300

    Update Spawner.cs
    Cache WaitSeconds and remove StopCoroutine OnDisable

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,31 +9,33 @@ public class Spawner : MonoBehaviour
     [SerializeField, Min(MinInterval)] private float _spawnInterval = MinInterval;
     [SerializeField] private EnemyPool _enemyPool;
 
-    private Coroutine _coroutine;
+    private WaitForSeconds _waitSeconds;
+
+    private void Awake()
+    {
+        _waitSeconds = new(_spawnInterval);
+    }
 
     private void OnEnable()
     {
         _enemyPool.Getted += OnGetted;
         _enemyPool.Released += OnReleased;
-        _coroutine = StartCoroutine(Spawn());
+        StartCoroutine(Spawn());
     }
 
     private void OnDisable()
     {
         _enemyPool.Getted -= OnGetted;
         _enemyPool.Released -= OnReleased;
-        StopCoroutine(_coroutine);
     }
 
     private IEnumerator Spawn()
     {
-        WaitForSeconds waitSeconds = new(_spawnInterval);
-
         while (enabled)
         {
             Vector3 spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)].position;
             _enemyPool.GetEnemy(spawnPoint, Quaternion.identity);
-            yield return waitSeconds;
+            yield return _waitSeconds;
         }
     }
 
